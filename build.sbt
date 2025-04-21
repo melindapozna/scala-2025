73e8358a7aa84a9fa9cf6694ad7343d488eb6dad
melindapozna <melindapozna@gmail.com>
2025-04-21 07:22:46 +0300

    Implement basic HTTP request

--- build.sbt
+++ build.sbt
@@ -4,5 +4,9 @@ ThisBuild / scalaVersion := "3.3.5"
 
 lazy val root = (project in file("."))
   .settings(
-    name := "scala-final-project-2025"
+    name := "scala-final-project-2025",
+    libraryDependencies ++= Seq("com.softwaremill.sttp.client4" %% "core" % "4.0.0-M7",
+      "au.com.onegeek" % "sbt-dotenv" % "2.1.192"
+    )
+
   )
--- src/main/scala/APIconnection/APIconnection.scala
+++ src/main/scala/APIconnection/APIconnection.scala
@@ -1,5 +1,31 @@
 package APIconnection
 
-object APIconnection {
+import sttp.client4.Response
+import sttp.client4.quick.*
+
+object APIconnection extends App{
   //send api request
+
+  //247 - solar
+
+  def requestData(datasetId: Int, startTime: String, endTime: String): Unit = {
+
+    val fileName = "apikey.txt"
+    val bufferedSource = scala.io.Source.fromFile(fileName)
+    val apiKey= bufferedSource.getLines().mkString.strip
+    bufferedSource.close()
+    println(apiKey)
+
+    val response: Response[Either[String, String]] = basicRequest
+      .get(uri"https://data.fingrid.fi/api/datasets/${datasetId}/data?startTime=${startTime}&endTime=${endTime}&format=json&pageSize=100")
+      .header("x-api-key", apiKey)
+      .send()
+
+    val parsedResponse = response.body match {
+      case Right(response) => println(response)
+      case Left(err) => println(s"Invalid request : $err")
+    }
+  }
+  //test request
+  //requestData(247, "2025-04-20T10:00:00", "2025-04-20T12:00:00")
 }
