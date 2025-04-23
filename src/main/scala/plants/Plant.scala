36e2aed35e4153f16125073c68be0bfd4d7c3b84
melindapozna <melindapozna@gmail.com>
2025-04-23 03:44:59 +0300

    Make getting ids more functional

--- src/main/scala/plants/Plant.scala
+++ src/main/scala/plants/Plant.scala
@@ -8,15 +8,19 @@ import java.util.TimerTask
 
 case object Plant {
   private val solar1 = new SolarPanel(IdProvider.setNextId())
-  private val solar1Sensor = new SolarSensor(solar1.id)
+  private val solar1Sensor = new SolarSensor(getPlantId(solar1))
 
   private val solar2 = new SolarPanel(IdProvider.setNextId())
-  private val solar2Sensor = new SolarSensor(solar2.id)
+  private val solar2Sensor = new SolarSensor(getPlantId(solar2))
 
   private val solarPanels = List(solar1, solar2)
   private val solarSensors = List(solar1Sensor, solar2Sensor)
 
   private val timer = new Timer()
+
+  private def getPlantId(plant: GeneralPlant): Int = {
+    plant.id
+  }
 
   def run(): Unit = {
     val getNewSensorData = new TimerTask {
