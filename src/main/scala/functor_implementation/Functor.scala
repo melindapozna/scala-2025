84a03bf361c266dd688fd787c079622e707991ee
melindapozna <melindapozna@gmail.com>
2025-05-03 15:34:41 +0300

    Remove functors from project

--- src/main/scala/functor_implementation/Functor.scala
+++
@@ -1,6 +0,0 @@
-package functor_implementation
-
-//functor[F[_]] so we can also use it with the functor aid
-trait Functor[F[_]] {
-  def map[A, B](fa: F[A])(f: A => B): F[B]
-}
--- src/main/scala/functor_implementation/FunctorAid.scala
+++
@@ -1,7 +0,0 @@
-package functor_implementation
-
-//FunctorAid is a helper that makes using the functor easier.
-// can call fmap(myData)(myFunction) and scala applies the right Functor (List, Either, Option, etc...)
-object FunctorAid {
-    def fmap[F[_], A, B](fa: F[A])(f: A => B)(using functor: Functor[F]): F[B] = functor.map(fa)(f)
-}
